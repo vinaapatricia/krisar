e21ec6ce7cf0a8dd949f2252e0d538b403f658ca
vinaapatricia <95381061+vinaapatricia@users.noreply.github.com>
2023-06-26 02:28:00 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,26 +1,35 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "grafkom";
+
+// Membuat koneksi ke database
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Memeriksa koneksi
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+// Memeriksa apakah form telah disubmit
 if (isset($_POST['submit'])) {
     $kritik = $_POST['kritik'];
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "aranimal";
+    // Menghindari serangan injeksi SQL dengan menggunakan prepared statement
+    $stmt = $conn->prepare("INSERT INTO tb_krisar (krisar) VALUES (?)");
+    $stmt->bind_param("s", $kritik);
 
-    $connect = new mysqli($servername, $username, $password, $database);
-
-    if ($connect->connect_error) {
-        die("Koneksi gagal: " . $connect->connect_error);
-    }
-
-    $sql = "INSERT INTO tb_krisar (data) VALUES ('$data')";
-
-    if ($connect->query($sql) === TRUE) {
+    if ($stmt->execute()) {
         echo "Kritik berhasil dikirim.";
     } else {
-        echo "Terjadi kesalahan: " . $connect->error;
+        echo "Terjadi kesalahan: " . $stmt->error;
     }
+    
 
-    $connect->close();
+    $stmt->close();
 }
+
+// Menutup koneksi
+mysqli_close($conn);
 ?>
